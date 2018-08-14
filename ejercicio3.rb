8fdf3f19583d13859fe2f67fcdd963fd0a9b2d25
mfssanhueza <mfssanhueza@gmail.com>
2018-08-14 19:59:14 -0300

    ejercicio3

---
+++ ejercicio3.rb
@@ -0,0 +1,31 @@
+class Vehicle
+  def initialize(model, year)
+    @model = model
+    @year = year
+    @start = false
+  end
+
+  def engine_start
+    @start = true
+  end
+end
+class Car < Vehicle
+  @@counter = 0
+  def initialize(model, year)
+    super
+    @@counter += 1
+  end
+  def self.contador
+    @@counter
+  end
+  def engine_start
+    super
+    print '¡El motor se ha encendido!'
+  end
+end
+
+
+10.times { Car.new('x', 'y') }
+c = Car.new('Toyota', '2010')
+c.engine_start
+puts Car.contador
